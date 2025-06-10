75dab9735c6fbfa1c3ed834295440b6d6aa6856e
Yordin Figueroa <yordinfigueroa2006@gmail.com>
2025-06-10 07:04:01 -0500

    Subiendo primeros cambios

---
+++ EjercicioLogico/README.md
@@ -0,0 +1,5 @@
+# Yordin Mauricio Figueroa Perez - 192268
+
+# Prueba Final Lógica – Control del Laboratorio
+
+# Documentacion Ejercicio
---
+++ EjercicioLogico/src/ControlDeLaboratorio.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ControlDeLaboratorio {
+
+    public static double promedioTemperaturasBajas(int[][] matriz){
+
+    }
+
+    public static int contarTemperaturasAltas(int[][] matriz){
+        for 
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int[][] temperaturas = new int[4][5];
+        System.out.println("Ingrese la temperatura de los equipos");
+        for(int i = 0; i < )
+    }
+}
